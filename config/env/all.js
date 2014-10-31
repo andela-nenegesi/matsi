e49438810e38865d0ff6ad4a276ca275ae85a6d9
Oni Kehinde Omowumi <Kehinde@Onis-MacBook-Pro.local>
2014-10-31 14:01:37 +0100

    Terwase using Kenny's MacBook added the dependencies to all.js and config.js

--- config/env/all.js
+++ config/env/all.js
@@ -17,6 +17,7 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
 			],
 			js: [
+				'public/lib/angular-payments/lib/angular-payments.js',
 				'public/lib/ng-file-upload/angular-file-upload-shim.min.js',
 				'public/lib/angular/angular.js',
 				'public/lib/ng-file-upload/angular-file-upload.min.js',
